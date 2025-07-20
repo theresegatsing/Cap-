ae56a2c243975a02e14d7903120aaddffa56f1d5
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-07-20 02:00:37 -0400

    commit

--- src/components/APIForm.jsx
+++ src/components/APIForm.jsx
@@ -4,8 +4,30 @@ const APIForm = ({inputs, handleChange, onSubmit}) => {
     <div>
       <h2> Select Your Image Attributes: </h2>
       <form className="form-container">
+        {inputs &&
+            Object.entries(inputs).map(([category, value], index) => (
+            <li className="form" key={index}>
+                <h2>{category} </h2>
+                <input
+                type="text"
+                name={category}
+                value={value}
+                placeholder="Input this attribute..."
+                onChange={handleChange}
+                className="textbox"
+                />
+                <br></br>
+                <br></br>
+                <p> {inputsInfo[index]}</p>
+            </li>
+        ))}
         
       </form>
+      <button type = "submit" className="button" onClick={onSubmit}>
+        Take the Pic!
+
+      </button>
+    
     </div>
   );
 };
