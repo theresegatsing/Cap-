cd17a0dda59cfea936e47fbe6ce7e73837aba4e2
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-07-20 02:10:51 -0400

    API form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import APIForm from './components/APIForm.jsx';
 
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
@@ -15,7 +16,25 @@ function App() {
     height: "",
   });
 
+  const submitForm = () => {}
+
   return (
+    <div className='whole-page'>
+      <h1> Build Your Own Screenshot! </h1>
+
+      <APIForm
+      inputs={inputs}
+      handleChange={(e) =>
+        setInputs((prevState) => ({
+          ...prevState,
+          [e.target.name]: e.target.value.trim(),
+        }))
+      }
+      onSubmit={submitForm}
+      />
+      <br></br>
+
+    </div>
     
   )
 }
--- src/components/APIForm.jsx
+++ src/components/APIForm.jsx
@@ -1,3 +1,12 @@
+const inputsInfo = [
+  "Input a link to any website you would like to take a screenshot of. Do not include https or any protocol in the URL",
+  "Input which image format you would prefer for your screenshot: jpeg, png, or webp",
+  "Input true or false if you would like your website screenshot to not contain any ads",
+  "Input true or false if you would like your website screenshot to not contain of those annoying 'allow cookies' banners",
+  "Choose the width of your screenshot (in pixels)",
+  "Choose the height of your screenshot (in pixels)",
+];
+
 const APIForm = ({inputs, handleChange, onSubmit}) => {
 
   return (
@@ -16,7 +25,6 @@ const APIForm = ({inputs, handleChange, onSubmit}) => {
                 onChange={handleChange}
                 className="textbox"
                 />
-                <br></br>
                 <br></br>
                 <p> {inputsInfo[index]}</p>
             </li>
