04a69850323625188a9a6b695579eaca8be813bc
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-07-20 01:51:42 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 function App() {
   const [count, setCount] = useState(0)
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
 
   return (
     
---
+++ src/components/APIForm.jsx
@@ -0,0 +1,13 @@
+const APIForm = ({inputs, handleChange, onSubmit}) => {
+
+  return (
+    <div>
+      <h2> Select Your Image Attributes: </h2>
+      <form className="form-container">
+        
+      </form>
+    </div>
+  );
+};
+
+export default APIForm;
